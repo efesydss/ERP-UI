9b86d5d098f0ca24aec10c5a77e4d292e8a9504a
efesydss <40852666+efesydss@users.noreply.github.com>
2025-03-28 12:01:14 +0300

    removed unused files

--- src/routes/_authenticated/admin/depots/new.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { DepotAdd } from '@/components/Admin/Depot/DepotAdd'
-
-export const Route = createFileRoute('/_authenticated/admin/depots/new')({
-  component: () => <DepotAdd/>
-})
-
